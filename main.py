14442b0393d98e5ff8ad0eef4409c0fc36599096
chanyoo-93 <chanyoo93@gmail.com>
2022-10-11 13:53:15 +0900

    main.py update

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import copy
+import time
+import csv
+import numpy as np
+os.environ['OPENCV_IO_MAX_IMAGE_PIXELS'] = pow(2, 40).__str__()
+import cv2
+from utils import calc_overlap, save_crop_images, crop_images, listup_files, COLOR
+
+
+# constant setting. / color, font
+COLOR    = COLOR
+STR_FONT = cv2.FONT_HERSHEY_SIMPLEX
+
+
+class TrainTestSplit:
+    """
+    """
+
+
+class CropImages:
+    """
+    """
+
+
+class MultipleTemplateMatching:
+    """
+    """
+
+
+class DetectByYolo:
+    """
+    """
+
+
+class DetectCropByYolo:
+    """
+    """
