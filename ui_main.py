b3cef21832a05102b3b630c0668480a1ed2a6e1e
chanyoo-93 <chanyoo93@gmail.com>
2022-10-28 17:27:30 +0900

    클래스명 수정

--- ui_main.py
+++ ui_main.py
@@ -1,27 +1,30 @@
 import os, sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+import utils
 
 def resource_path(relative_path):
     base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
-form = resource_path('StartWindow.ui')
+form = resource_path('MainWindow.ui')
 form_class = uic.loadUiType(form)[0]
 
-form_main = resource_path('MainWindow.ui')
+form_main = resource_path('SecondWindow.ui')
 form_mainwindow = uic.loadUiType(form_main)[0]
 
 def init_project_dir():
-    basic_dir : dict = {
+    base_dir : dict = {
         "cfg"    : "Config",
-        "train"  : "Train",
-        "test"   : "Test",
-        "val"    : "Valid",
+        "train"  : "TrainData",
+        "test"   : "TestData",
+        "val"    : "ValidData",
         "results": "Results"}
-    return basic_dir
+    return base_dir
+base_dir = init_project_dir()
 
-class StartWindow(QMainWindow, form_class):
+class MainWindow(QMainWindow, form_class):
 
     def __init__(self):
         super().__init__()
@@ -29,20 +32,18 @@ class StartWindow(QMainWindow, form_class):
 
     def push_open_project(self):
         dir_name = QFileDialog.getExistingDirectory(self)
-        in_dir_list = os.listdir(dir_name)
-        print(in_dir_list)
-        self.btn_main_to_second()
 
     def push_create_project(self):
         dir_name = QFileDialog.getExistingDirectory(self)
-        basic_dir = init_project_dir()
-        for name in basic_dir:
-            os.mkdir(os.path.join(dir_name, basic_dir[name]))
+        for name in base_dir:
+            os.mkdir(os.path.join(dir_name, base_dir[name]))
+
+    def push_start_train(self):
         self.btn_main_to_second()
 
     def btn_main_to_second(self):
         self.hide()
-        self.second = MainWindow()
+        self.second = SecondWindow()
         self.second.exec()
         self.show()
 
@@ -50,9 +51,10 @@ class StartWindow(QMainWindow, form_class):
         self.close()
 
 
-class MainWindow(QDialog, QWidget, form_mainwindow):
+class SecondWindow(QDialog, QWidget, form_mainwindow):
+
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super(SecondWindow, self).__init__()
         self.initUi()
         self.show()
 
@@ -64,6 +66,6 @@ class MainWindow(QDialog, QWidget, form_mainwindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    myWindow = StartWindow()
+    myWindow = MainWindow()
     myWindow.show()
     app.exec_()
